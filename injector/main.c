d37bf8116271635a9043be865e5de0bda9f1fc6f
rodionovd <i.am.rodionovd@gmail.com>
2014-07-27 18:18:43 +0700

    Prevent helper daemon from exiting in the middle of injection
    Invalidate an idle-exit timer in the helper before starting an injection and reset it just after the injection

--- injector/main.c
+++ injector/main.c
@@ -11,7 +11,10 @@
 #include "rd_inject_library.h"
 
 #define kIdleExitTimeoutSec (10)
+#define kDefaultIdleTime (dispatch_time(DISPATCH_TIME_NOW, kIdleExitTimeoutSec * NSEC_PER_SEC))
 #define kDeamonIdentifer "me.rodionovd.RDInjectionWizard.injector"
+
+static dispatch_source_t idle_exit_timer = NULL;
 
 static bool main_routine(xpc_object_t dictionary)
 {
@@ -31,12 +34,18 @@ static void __XPC_Connection_Handler(xpc_connection_t connection)  {
 	xpc_connection_set_event_handler(connection, ^(xpc_object_t event) {
         if (xpc_get_type(event) != XPC_TYPE_ERROR) {
 
-            bool success = main_routine(event);
-            xpc_connection_t remote = xpc_dictionary_get_remote_connection(event);
-            xpc_object_t reply = xpc_dictionary_create_reply(event);
-            xpc_dictionary_set_bool(reply, "status", success);
-            xpc_connection_send_message(remote, reply);
-            xpc_release(reply);
+            /* Pause an idle-exit timer */
+            dispatch_source_set_timer(idle_exit_timer, DISPATCH_TIME_FOREVER, 0, 0);
+            {
+                bool success = main_routine(event);
+                xpc_connection_t remote = xpc_dictionary_get_remote_connection(event);
+                xpc_object_t reply = xpc_dictionary_create_reply(event);
+                xpc_dictionary_set_bool(reply, "status", success);
+                xpc_connection_send_message(remote, reply);
+                xpc_release(reply);
+            }
+            /* Reset the timer */
+            dispatch_source_set_timer(idle_exit_timer, kDefaultIdleTime, 0, 0);
         } else {
             // error handling?
         }
@@ -60,17 +69,16 @@ int main(__unused int argc, __unused const char *argv[]) {
     });
 
     /* Set up an idle-exit timer. @see kIdleExitTimeoutSec for timeout value. */
-    dispatch_source_t timer_source = dispatch_source_create(DISPATCH_SOURCE_TYPE_TIMER, 0, 0,
-                                                            dispatch_get_main_queue());
-    if (!timer_source) {
+    idle_exit_timer = dispatch_source_create(DISPATCH_SOURCE_TYPE_TIMER, 0, 0,
+                                          dispatch_get_main_queue());
+    if (!idle_exit_timer) {
         syslog(LOG_NOTICE, "Failed to create an idle-exit timer source.");
         exit(EXIT_FAILURE);
     }
-    dispatch_set_context(timer_source, NULL);
-    dispatch_source_set_event_handler_f(timer_source, (void (*)(void *))exit);
-    dispatch_time_t time = dispatch_time(DISPATCH_TIME_NOW, kIdleExitTimeoutSec * NSEC_PER_SEC);
-    dispatch_source_set_timer(timer_source, time, 0, 0);
-    dispatch_resume(timer_source);
+    dispatch_set_context(idle_exit_timer, NULL);
+    dispatch_source_set_event_handler_f(idle_exit_timer, (void (*)(void *))exit);
+    dispatch_source_set_timer(idle_exit_timer, kDefaultIdleTime, 0, 0);
+    dispatch_resume(idle_exit_timer);
 
     xpc_connection_resume(service);
     dispatch_main();
